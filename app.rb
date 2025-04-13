a6cd551a6b536147ea651147e23d1b334951d12b
raheeljafarani <rjafaran@chicagobooth.edu>
2025-04-13 14:53:07 +0000

    rock page display with color

--- app.rb
+++ app.rb
@@ -7,3 +7,22 @@ get("/") do
   <p>Define some routes in app.rb</p>
   "
 end
+
+get("/rock") do
+
+  moves = ["rock", "paper", "scissors"]
+
+  @comp_move = moves.sample
+
+  if @comp_move == "rock"
+    @outcome = "tied"
+
+elsif @comp_move == "paper"
+  @outcome = "lost"
+else
+  @outcome = "won"
+end
+
+erb(:zebra)
+  
+end
